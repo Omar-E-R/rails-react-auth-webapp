89de164914a0a61e8f946565f44896793494efb9
Omar <32805384+Omar-E-R@users.noreply.github.com>
2021-10-27 14:08:56 +0200

    Revert "Omar e r feature readme.md (#4)"
    This reverts commit fb0604cba4eb82bf4dfacfaaa76be5e0293c2cc8.

--- config/initializers/session_store.rb
+++
@@ -1,5 +0,0 @@
-if Rails.env === 'production'
-  Rails.application.config.session_store :cookie_store, key: '_rails-react-auth-webapp', domain: :all, tld_length: 2
-else
-  Rails.application.config.session_store :cookie_store, key: '_rails-react-auth-webapp'
-end
